0b7155852e2231e771be2559d07830d98fb153b4
Maria Berschauer <maria.berschauer@zonarsystems.com>
2021-05-30 12:18:19 -0700

    The codes were updated

--- blogging/models.py
+++ blogging/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib import admin
 
 
 class Post(models.Model):
@@ -19,6 +20,7 @@ class Category(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
     posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+    exclude = ('posts',)
 
     def __str__(self):
         return self.name
--- blogging/views.py
+++ blogging/views.py
@@ -1,32 +1,22 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import View
 
 
 # Create your views here.
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+class StubView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse(body, content_type="text/plain")
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'list.html', context)
+class PostListView(ListView):
+    model = 'post_list'
+    queryset = Post.objects.filter(published_date__exact=None)
+    template_name = 'blogging/list.html'
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
