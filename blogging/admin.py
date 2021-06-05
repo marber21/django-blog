0c470fdd596483dbd23f46be7ebdb25a8b07da83
Maria Berschauer <maria.berschauer@zonarsystems.com>
2021-06-04 19:25:17 -0700

    Added files.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+
 # Register your models here.
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+
+
+class PostAdmin(admin.ModelAdmin):
+    Inlines = [
+        CategoryInline,
+
+    ]
+
+
 admin.site.register(Post)
 admin.site.register(Category)
